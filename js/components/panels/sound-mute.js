2daf7e5a69867379638bde25ba3ac4aa86cf7141
Philpp Bisson <philipp.bisson@gmail.com>
2014-02-25 23:28:12 +0100

    fix mute

--- js/components/panels/sound-mute.js
+++ js/components/panels/sound-mute.js
@@ -3,13 +3,19 @@
 define(['react'], function(React) {
 	var SoundMinus = React.createClass({
 		clickHandler:function() {
-      var m=!this.props.data.mute;
-      $.get('/cgi-bin/set?/sound/mute=' + m);
+      var m=this.props.data.mute;
+
+      if (m == "false") {
+	      $.get('/cgi-bin/set?/sound/mute=true');
+      }else{
+  	    $.get('/cgi-bin/set?/sound/mute=false');
+      }
 		},
 		render: function() {
-
+			var classMute = ""
 			if (typeof this.props.data !== 'undefined') {
-      var classMute = (this.props.data.mute != 'true') ? 'isPlaying' : '';
+      	//console.log(this.props.data.mute);
+      	var classMute = (this.props.data.mute == 'true') ? 'isPlaying' : '';
 			}
 
 			return ( <span className= {'glyphicon glyphicon-volume-off '+ classMute } onClick={this.clickHandler}></span>);
